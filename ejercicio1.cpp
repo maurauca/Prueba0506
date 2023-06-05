16d3f9e22ba2c92518b4f8ef9c658959d0c536e8
maurauca <maura.quintanilla15903@est.uca.edu.ni>
2023-06-05 08:16:49 -0600

    completado

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -12,4 +12,36 @@ calcule la suma de todos sus elementos.
 #include <iostream>
 using namespace std;
 
+int sumaArreglo(int arr[], int tam) {
+    int sum = 0;
+    for (int i = 0; i < tam; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int tam;
+    cout << "Ingrese el tamano del arreglo: ";
+    cin >> tam;
+
+    if (tam <= 0) {
+        cout << "El tamano del arreglo debe ser mayor a cero y no debe de ser negativo." << endl;
+        return 0;
+    }
+    
+    int* arr = new int[tam];
+
+    cout << "Ingrese los números del arreglo:" << endl;
+    for (int i = 0; i < tam; i++) {
+        cout << "Numero " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    int sum = sumaArreglo(arr, tam);
+    cout << "La suma de los elementos del arreglo es: " << sum << endl;
+
+    return 0;
+}
+
 
