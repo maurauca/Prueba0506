fb74fa4a13af277bfa6b57befb7312ced99ca004
maurauca <maura.quintanilla15903@est.uca.edu.ni>
2023-06-05 07:29:06 -0600

    ejercicio 1

---
+++ ejercicio1.cpp
@@ -0,0 +1,15 @@
+/*
+1. Ejercicio: Suma de los elementos de un arreglo
+ Descripción: Escribe un programa que reciba como entrada un arreglo de números enteros y
+calcule la suma de todos sus elementos.
+ Requisitos:
+ - Utilizar un ciclo para recorrer el arreglo.
+ - Utilizar una función para realizar la suma.
+ - Utilizar una bifurcación para manejar el caso de un arreglo vacío.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
